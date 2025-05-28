70a9491f6fa4408d7b3319a30f522aab3d8f9dcd
JMonkeyGamesInc <jmonkeygamesinc@gmail.com>
2025-05-28 09:17:12 -0400

    Importing Utility Classes.  Making UI

---
+++ app/src/main/java/GameShopEngine/GameShopUI/GameShopUIPolyMesh.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GameShopEngine.GameShopUI;
+
+import GameShopEngine.GameShopATMS;
+import GameShopEngine.GameShopPolyMesh;
+import GameShopEngine.GameShopPolySurface;
+
+/**
+ *
+ * @author jmonkeygamesinc
+ */
+public class GameShopUIPolyMesh extends GameShopPolyMesh {
+    
+    public GameShopUIPolyMesh(GameShopPolySurface[] gspSurfaces, GameShopATMS atms) {
+        super(gspSurfaces, atms);
+    }
+    
+     
+}
---
+++ app/src/main/java/GameShopEngine/GameShopUI/Widgets/Omnis/GameShopTab.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GameShopEngine.GameShopUI.Widgets.Omnis;
+
+/**
+ *
+ * @author jmonkeygamesinc
+ */
+public class GameShopTab {
+    
+}
